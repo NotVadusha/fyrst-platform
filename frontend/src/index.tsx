8d5b9d6ac1aa0d856277bdaa77721fddbdbd1dd3
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-17 20:09:10 +0300

    Update routes to react routes v6.15 standart

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from './store/store';
+import { RouterProvider } from 'react-router-dom';
+import store from './store';
+import { router } from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ frontend/src/routes.tsx
@@ -0,0 +1,149 @@
+import { createBrowserRouter } from 'react-router-dom';
+import BookingPage from './pages/BookingPage';
+import TimeCardPage from './pages/TimeCard';
+import ProfilePage from './pages/ProfilePage';
+import MessangerPage from './pages/MessangerPage';
+import PaymentsPage from './pages/PaymentsPage';
+import AuthPage from './pages/AuthPage';
+import EmployesPage from './pages/EmployesPage';
+import ErrorPage from './pages/ErrorPage';
+import TestPage from './pages/TestPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Layout from './pages/Layout';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+
+    children: [
+      {
+        path: 'booking',
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <BookingPage />,
+          },
+          {
+            path: ':id',
+            element: <TestPage />,
+          },
+          {
+            path: 'create',
+            element: <TestPage />,
+          },
+        ],
+      },
+      {
+        path: 'timecard',
+        children: [
+          {
+            index: true,
+            element: <TimeCardPage />,
+          },
+          {
+            path: ':id',
+            element: <TestPage />,
+          },
+          {
+            path: 'create',
+            element: <TestPage />,
+          },
+        ],
+      },
+      {
+        path: 'profile',
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <ProfilePage />,
+          },
+          {
+            path: ':id',
+            element: <TestPage />,
+          },
+          {
+            path: 'edit',
+            element: <TestPage />,
+          },
+          {
+            path: 'notifications',
+            element: <TestPage />,
+          },
+          {
+            path: 'security',
+            element: <TestPage />,
+          },
+        ],
+      },
+      {
+        path: 'messanger',
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <MessangerPage />,
+          },
+        ],
+      },
+      {
+        path: 'payment',
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <PaymentsPage />,
+          },
+          {
+            path: ':id',
+            element: <TestPage />,
+          },
+        ],
+      },
+      {
+        path: 'Employe',
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <EmployesPage />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: 'login',
+        element: <AuthPage />,
+      },
+      {
+        path: 'forgot',
+        element: <TestPage />,
+      },
+      {
+        path: 'reset',
+        element: <TestPage />,
+      },
+      {
+        path: 'reset/:key',
+        element: <TestPage />,
+      },
+      {
+        path: 'signup',
+        element: <TestPage />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+]);
