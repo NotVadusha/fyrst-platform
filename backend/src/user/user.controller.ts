cad4c41aff25115039af099eeb682cd9bfd95df9
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-18 21:36:38 +0300

    Add validation refactor user service

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+  ValidationPipe,
+  BadRequestException,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto';
 
@@ -7,7 +18,31 @@ export class UserController {
   constructor(private readonly UserService: UserService) {}
 
   @Post()
-  create(@Body() UserInfo: CreateUserDto) {
-    return this.UserService.create();
+  async create(@Body(ValidationPipe) userInfo: CreateUserDto) {
+    return await this.UserService.create(userInfo);
+  }
+  @Get('/:id')
+  async getOne(@Param(ParseIntPipe) userId: number) {
+    const user = this.UserService.findOne(userId);
+    if (!user) throw new BadRequestException("This user doesn't exist");
+    return user;
+  }
+  @Get()
+  async getAll() {
+    return this.UserService.findAll();
+  }
+  @Patch('/:id')
+  async update(@Param(ParseIntPipe) userId: number, @Body(ValidationPipe) userInfo: CreateUserDto) {
+    const updatedUser = await this.UserService.update(userInfo, userId);
+    if (updatedUser.length < 1) throw new BadRequestException("This user doesn't exist");
+
+    return this.UserService.findOne(userId);
+  }
+  @Delete('/:id')
+  async delete(@Param(ParseIntPipe) userId: number) {
+    const deleteStatus = await this.UserService.delete(userId);
+    if (!deleteStatus) throw new BadRequestException("This user doesn't exist");
+
+    return { Message: 'Successfully deleted' };
   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,27 +1,29 @@
+import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './entities';
+import { UpdateUserDto } from './dto';
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
 
-  async create() {
-    return this.userRepository.create();
+  async create(userInfo: CreateUserDto) {
+    return await this.userRepository.create({ ...userInfo });
   }
 
   async findAll() {
-    return 2;
+    return await this.userRepository.findAll();
   }
 
-  async findOne() {
-    return 3;
+  async findOne(userId: number) {
+    return await this.userRepository.findOne({ where: { id: userId } });
   }
 
-  async update() {
-    return 4;
+  async update(updateInfo: UpdateUserDto, userId: number) {
+    return await this.userRepository.update({ ...updateInfo }, { where: { id: userId } });
   }
 
-  async remove() {
-    return 5;
+  async delete(userId: number) {
+    return await this.userRepository.destroy({ where: { id: userId } });
   }
 }
