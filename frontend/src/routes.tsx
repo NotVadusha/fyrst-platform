5c56b8ab05b0038b8b5e047c2fb20ba743ee2681
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-18 08:30:13 +0300

    fixed "react must be in scope" error

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import BookingPage from './pages/BookingPage';
 import TimeCardPage from './pages/TimeCard';
