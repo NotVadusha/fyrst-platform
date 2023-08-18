0798fe805dac0daad116939cd13b2dd624d0ce2c
NotVadusha <118393076+NotVadusha@users.noreply.github.com>
2023-08-18 10:27:46 +0300

    Feature/routes and redux (#2)
    * Add patterned pages for routing
    
    * Add store and router provider
    
    * Add basic routes
    
    * initial commit
    
    * Update react-router 6.2.1 -> 6.15.0
    
    * Delete redunant file
    
    * Add test redux store
    
    * Update routes to react routes v6.15 standart
    
    * Add basic layout for routes
    
    * Update text in pages
    
    * fixed "react must be in scope" error
    
    * moved routes into seprate folder
    
    * Scattered pages into their folders

---
+++ frontend/src/pages/payments/PaymentsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentsPage = () => {
+  return (
+    <div className='h-128 bg-gray'>
+      <h1>Payments</h1>
+    </div>
+  );
+};
+
+export default PaymentsPage;
