71fbd7b4b6a29b25612fff83ca99096dd8464e13
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-18 22:22:52 +0300

    revert config

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -3,11 +3,11 @@ import { SequelizeModuleOptions } from '@nestjs/sequelize';
 import { User } from 'src/user';
 
 export const sequelizeDevelopmentConfig: SequelizeModuleOptions = {
-  username: 'postgres',
-  password: 'root',
-  database: 'postgres',
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   port: 5432,
-  host: 'localhost',
+  host: 'db',
   dialect: 'postgres',
   autoLoadModels: true,
   models: [User],
