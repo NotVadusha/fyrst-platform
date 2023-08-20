06242d8e52f621dfcfd5b88a6862756ce061badf
Anton Poroshkin <poroshkinanton18@gmail.com>
2023-08-20 16:58:09 +0300

    Remove createdAt from CreateTimecardDto

--- backend/src/timecard/dto/create-timecard.dto.ts
+++ backend/src/timecard/dto/create-timecard.dto.ts
@@ -1,11 +1,8 @@
-import { IsDate, IsNumber } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 export class CreateTimecardDto {
   @IsNumber()
   bookingId: number;
-
-  @IsDate()
-  createdAt: Date;
 
   @IsNumber()
   createdBy: number;
