938436d286c74a588f5fa23b6a5c793d40dce062
Anton Poroshkin <poroshkinanton18@gmail.com>
2023-08-20 18:02:57 +0300

    Add exceptiond handling for timecard controller

--- backend/src/timecard/timecard.controller.ts
+++ backend/src/timecard/timecard.controller.ts
@@ -1,46 +1,89 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
-  ParseIntPipe,
+  Get,
   Inject,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
 } from '@nestjs/common';
-import { CreateTimecardDto, UpdateTimecardDto } from './dto';
 import { TIMECARD_SERVICE_INJECTION_TOKEN } from './constants';
+import { CreateTimecardDto, UpdateTimecardDto } from './dto';
 import { ITimecardService } from './interfaces';
 
 @Controller('timecard')
 export class TimecardController {
+  private readonly logger = new Logger(TimecardController.name);
+
   constructor(
     @Inject(TIMECARD_SERVICE_INJECTION_TOKEN) private readonly timecardService: ITimecardService,
   ) {}
 
   @Post()
-  create(@Body() createTimecardDto: CreateTimecardDto) {
-    return this.timecardService.create(createTimecardDto);
+  async create(@Body() createTimecardDto: CreateTimecardDto) {
+    try {
+      const created = await this.timecardService.create(createTimecardDto);
+      this.logger.log('Successfully created timecard');
+      return created;
+    } catch (error) {
+      this.logger.error("Coudn't create timecard", error);
+      throw new InternalServerErrorException("Couldn't create timecard");
+    }
   }
 
   @Get()
-  findAll() {
-    return this.timecardService.getAll();
+  async findAll() {
+    try {
+      const timecards = await this.timecardService.getAll();
+      this.logger.log('Successfully got all timecards');
+      return timecards;
+    } catch (error) {
+      this.logger.error("Couldn't get timecards", error);
+      throw new InternalServerErrorException("Couldn't get timecards");
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.timecardService.getById(id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    try {
+      const timecard = await this.timecardService.getById(id);
+      this.logger.log('Successfully got timecard');
+      return timecard;
+    } catch (error) {
+      this.logger.error("Couldn't get timecard", error);
+      throw new NotFoundException('Requested timecard could not be found');
+    }
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateTimecardDto: UpdateTimecardDto) {
-    return this.timecardService.update(id, updateTimecardDto);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTimecardDto: UpdateTimecardDto,
+  ) {
+    try {
+      const updated = await this.timecardService.update(id, updateTimecardDto);
+      this.logger.log('Successfully updated timecard');
+      return updated;
+    } catch (error) {
+      this.logger.error("Couldn't update timecard", error);
+      throw new InternalServerErrorException("Couldn't update timecard");
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.timecardService.remove(id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    try {
+      const removed = await this.timecardService.remove(id);
+      this.logger.log('Successfully removed timecard');
+      return removed;
+    } catch (error) {
+      this.logger.error("Couldn't remove timecard", error);
+      throw new InternalServerErrorException("Couldn't remove timecard");
+    }
   }
 }
