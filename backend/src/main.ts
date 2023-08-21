5e35863f34639367347a485dc6cb348224ac325a
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-08-21 15:56:42 +0300

    added swagger in app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './config/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1', app, document);
+
   await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
