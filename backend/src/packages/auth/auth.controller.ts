0628a7c26bdd46486ad89b2487849255a18ba57e
sl-chern <slavikchernog@gmail.com>
2023-08-21 22:03:31 +0300

    added swagger documentation for reset password and authentication modules

--- backend/src/packages/auth/auth.controller.ts
+++ backend/src/packages/auth/auth.controller.ts
@@ -6,38 +6,55 @@ import { AccessTokenGuard } from './guards/access-token.guard';
 import { GoogleOauthGuard } from './guards/google.guard';
 import { Response } from 'express';
 import { CreateUserDto } from 'src/packages/user/dto/create-user.dto';
+import { ApiHeader, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { MessageResponse, TokenResponse } from 'src/helpers/responceClasses';
 
+@ApiTags('Authorization and authentication endpoints')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'User registration' })
+  @ApiResponse({ status: 201, type: MessageResponse })
   @Post('registration')
   async registration(@Body() createUserDto: CreateUserDto) {
     return await this.authService.registration(createUserDto);
   }
 
+  @ApiOperation({ summary: 'User login' })
+  @ApiResponse({ status: 200, type: TokenResponse })
   @Post('login')
   @HttpCode(200)
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);
   }
 
+  @ApiOperation({ summary: 'User logout' })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'JWT token',
+  })
   @UseGuards(AccessTokenGuard)
   @Get('logout')
   async logout(@Request() req) {
     await this.authService.logout(req.user['id']);
   }
 
+  @ApiOperation({ summary: 'Refresh JWT token' })
+  @ApiResponse({ status: 200, type: TokenResponse })
   @Post('refresh')
   @HttpCode(200)
   async refresh(@Body() refreshDto: RefreshDto) {
     return await this.authService.refresh(refreshDto);
   }
 
+  @ApiOperation({ summary: 'Signin by Google' })
   @Get('google')
   @UseGuards(GoogleOauthGuard)
   async googleLogin() {}
 
+  @ApiOperation({ summary: 'Google callback after success authentication' })
+  @ApiResponse({ status: 302 })
   @Get('google/callback')
   @UseGuards(GoogleOauthGuard)
   async googleCallback(@Request() req, @Res() res: Response) {
--- backend/src/packages/reset-password/reset-password.controller.ts
+++ backend/src/packages/reset-password/reset-password.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, HttpCode } from '@nestjs/common';
 import { ResetPasswordService } from './reset-password.service';
 import { ResetPasswordDto } from './dto/reset-password.dto';
 import { NewPasswordDto } from './dto/new-password.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { MessageResponse } from 'src/helpers/responceClasses';
 
+@ApiTags('Reset password endpoints')
 @Controller('reset-password')
 export class ResetPasswordController {
   constructor(private resetPasswordService: ResetPasswordService) {}
 
+  @ApiOperation({ summary: 'Sending email to reset password' })
+  @ApiResponse({ status: 200, type: MessageResponse })
+  @HttpCode(200)
   @Post()
   async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
     return await this.resetPasswordService.resetPassword(resetPasswordDto);
   }
 
+  @ApiOperation({ summary: 'Setting a new password' })
+  @ApiResponse({ status: 200, type: MessageResponse })
+  @HttpCode(200)
   @Post('new-password')
   async updatePassword(@Body() newPasswordDto: NewPasswordDto) {
     return await this.resetPasswordService.updatePassword(newPasswordDto);
