296ed66a5f7ceebd4b2b7b507d5dd4247c0e5ec4
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-08-21 18:03:40 +0300

    removed swagger api property from dtos

--- backend/src/packages/user/dto/create-user.dto.ts
+++ backend/src/packages/user/dto/create-user.dto.ts
@@ -1,45 +1,35 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 import { IsOptional, IsString, IsDate, IsBoolean, IsNotEmpty, IsEmail } from 'class-validator';
 import { Transform } from 'class-transformer';
 export class CreateUserDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   first_name: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   last_name: string;
 
-  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
   @IsString()
   @IsOptional()
   phone_number?: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   city: string;
 
-  @ApiProperty()
   @Transform(({ value }) => new Date(value))
   @IsDate()
   @IsNotEmpty()
   birthdate: Date;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
 
-  @ApiProperty()
   @IsBoolean()
   @IsNotEmpty()
   is_confirmed: boolean;
