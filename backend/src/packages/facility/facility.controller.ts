392b7696689a99925a19d03bb90f078fe2788f48
Ian <grytsov.ian@gmail.com>
2023-08-22 19:48:10 +0300

    Implemented FacilityController
    Added a FacilityController to handle CRUD operations for facilities.

--- backend/src/packages/facility/facility.controller.ts
+++ backend/src/packages/facility/facility.controller.ts
@@ -1,33 +1,84 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Logger,
+  BadRequestException,
+} from '@nestjs/common';
 import { FacilityService } from './facility.service';
 import { FacilityDto } from './dto/facility.dto';
 
 @Controller('facility')
 export class FacilityController {
+  private readonly logger = new Logger(FacilityController.name);
+
   constructor(private readonly facilityService: FacilityService) {}
 
   @Post()
-  create(@Body() facility: FacilityDto) {
-    return this.facilityService.create(facility);
+  async create(@Body() facility: FacilityDto) {
+    try {
+      const createdFacility = await this.facilityService.create(facility);
+      return createdFacility;
+    } catch (error) {
+      this.logger.error('Failed to create facility', error);
+      throw new BadRequestException('Failed to create facility');
+    }
   }
 
   @Get()
-  findAll() {
-    return this.facilityService.findAll();
+  async findAll() {
+    try {
+      const facilities = await this.facilityService.findAll();
+      return facilities;
+    } catch (error) {
+      this.logger.error('Failed to retrieve facilities', error);
+      throw new BadRequestException('Failed to retrieve facilities');
+    }
   }
 
   @Get(':id')
-  findById(@Param('id') id: number) {
-    return this.facilityService.findById(id);
+  async findById(@Param('id') id: number) {
+    try {
+      const facility = await this.facilityService.findById(id);
+      if (!facility) {
+        throw new BadRequestException(`Facility with ID ${id} not found`);
+      }
+      return facility;
+    } catch (error) {
+      this.logger.error(`Failed to retrieve facility with ID ${id}`, error);
+      throw new BadRequestException(`Failed to retrieve facility with ID ${id}`);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateFacility: FacilityDto) {
-    return this.facilityService.update(id, updateFacility);
+  async update(@Param('id') id: number, @Body() updateFacility: FacilityDto) {
+    try {
+      const updatedFacility = await this.facilityService.update(id, updateFacility);
+      if (!updatedFacility) {
+        throw new BadRequestException(`Facility with ID ${id} not found`);
+      }
+      return updatedFacility;
+    } catch (error) {
+      this.logger.error(`Failed to update facility with ID ${id}`, error);
+      throw new BadRequestException(`Failed to update facility with ID ${id}`);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.facilityService.remove(id);
+  async remove(@Param('id') id: number) {
+    try {
+      const deletedFacility = await this.facilityService.remove(id);
+      if (!deletedFacility) {
+        throw new BadRequestException(`Facility with ID ${id} not found`);
+      }
+      return deletedFacility;
+    } catch (error) {
+      this.logger.error(`Failed to delete facility with ID ${id}`, error);
+      throw new BadRequestException(`Failed to delete facility with ID ${id}`);
+    }
   }
 }
