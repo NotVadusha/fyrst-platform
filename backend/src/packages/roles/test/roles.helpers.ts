707dad5901a7b1a0c0c5a35dbc12c8b0f37f2972
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-22 14:08:10 +0300

    Fixed tests

--- backend/src/packages/roles/test/roles.helpers.ts
+++ backend/src/packages/roles/test/roles.helpers.ts
@@ -3,22 +3,22 @@ import { RoleDto } from '../dto/role.dto';
 export const rolesMock = (): TestRole[] => {
   return [
     {
-      role_id: 1,
+      id: 1,
       label: 'first_role',
     },
     {
-      role_id: 2,
+      id: 2,
       label: 'second_role',
     },
   ];
 };
 
 export interface TestRole extends RoleDto {
-  role_id: number;
+  id: number;
 }
 
 export const updateInfo = {
-  role_id: 1,
+  id: 1,
   label: 'updated_role',
 };
 
