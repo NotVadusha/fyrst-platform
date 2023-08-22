be92f18ba80e347867757d8eb93b758841375487
scape76 <danyaolekhq@gmail.com>
2023-08-22 16:33:31 +0300

    fix: make radio btn group work with custom form

--- frontend/src/components/ui/common/RadioButton/RadioButton.tsx
+++ frontend/src/components/ui/common/RadioButton/RadioButton.tsx
@@ -6,22 +6,29 @@ interface RadioButtonProps {
   size: 'small' | 'big';
   name: string;
   label: string;
-  checked: boolean;
+  // checked: boolean;
+  onChange?: (e: any) => void;
+  value?: string;
+  // checked: boolean;
 }
-
-const RadioButton = ({ size, name, label, checked }: RadioButtonProps) => {
-  return (
-    <label className={styles.container}>
-      <input
-        type='radio'
-        checked={checked}
-        name={name}
-        readOnly
-        className={`${styles.radio} ${styles[size]}`}
-      ></input>
-      <span className={styles.label}>{label}</span>
-    </label>
-  );
-};
+const RadioButton = React.forwardRef<HTMLInputElement, RadioButtonProps>(
+  ({ size, name, label, value, onChange, ...props }, ref) => {
+    return (
+      <label className={styles.container}>
+        <input
+          {...props}
+          ref={ref}
+          type='radio'
+          checked={value === label}
+          value={value === label ? 'on' : 'off'}
+          name={name}
+          onChange={() => onChange?.(label)}
+          className={`${styles.radio} ${styles[size]}`}
+        ></input>
+        <span className={styles.label}>{label}</span>
+      </label>
+    );
+  },
+);
 
 export default RadioButton;
