e31e4fbf3bcce64d7c5a97e3f85a50383fb971e5
sl-chern <slavikchernog@gmail.com>
2023-08-28 20:37:02 +0300

    created generating pdf service

--- microservices/invoice/src/packages/convert/convert.service.ts
+++ microservices/invoice/src/packages/convert/convert.service.ts
@@ -11,18 +11,29 @@ export class ConvertService implements OnModuleInit {
   private hbsTemplate: HandlebarsTemplateDelegate;
 
   async toPdfInvoice<T>(data: T) {
-    const invoiceHTML = this.hbsTemplate(data);
+    const invoiceHTML = this.hbsTemplate({ ...data, date: new Date().toLocaleDateString() });
     const pdfStream = await this.gotenbergClientService.createPdfFromHtml(invoiceHTML);
     pdfStream.pipe(fs.createWriteStream('src/packages/convert/pdfs/1.pdf'));
   }
 
   async onModuleInit(): Promise<void> {
     this.initTemplate();
+    this.registerHelpers();
   }
 
   private async initTemplate(): Promise<void> {
     const data = await fs.promises.readFile(this.templateFile, 'utf-8');
     const hbsTemplate = Handlebars.compile(data, { noEscape: true });
     this.hbsTemplate = hbsTemplate;
+  }
+
+  private async registerHelpers(): Promise<void> {
+    Handlebars.registerHelper('countAmount', (hours, price) => (hours * price).toFixed(2));
+    Handlebars.registerHelper('countTax', (hours, price, tax) =>
+      (((hours * price) / 100) * tax).toFixed(2),
+    );
+    Handlebars.registerHelper('countAmountTotal', (hours, price, tax) =>
+      (hours * price + ((hours * price) / 100) * tax).toFixed(2),
+    );
   }
 }
--- microservices/invoice/src/packages/invoice/dto/data-to-pdf.dto.ts
+++ microservices/invoice/src/packages/invoice/dto/data-to-pdf.dto.ts
@@ -1,1 +1,8 @@
-export class DataToPdfDto {}
+import { Booking, Facility, Timecard, User } from 'src/types';
+
+export class DataToPdfDto {
+  timecard: Timecard;
+  user: User;
+  booking: Booking;
+  facility: Facility;
+}
