ccfd34246b0c1e840fade5ebbcfc5df90f536ddf
Mariia Haiduchenko <ma.gaiduchenko@gmail.com>
2023-08-28 10:30:24 +0300

    added chat buttons styling

--- frontend/src/ui/common/Button/Button.tsx
+++ frontend/src/ui/common/Button/Button.tsx
@@ -12,9 +12,11 @@ export const buttonVariants = cva(
         secondary:
           'bg-white border text-blue border-solid border-blue hover:border-hover hover:text-hover active:border-dark-blue disabled:text-inactive disabled:border-inactive',
         tertiary: 'text-blue bg-transparent disabled:text-inactive',
+        message: 'bg-white text-dark-blue text-base rounded-lg shadow-md', //default-body-message
       },
       size: {
         default: 'h-12 py-4 px-8',
+        message: 'h-8	px-4 py-4',
       },
     },
     defaultVariants: {
