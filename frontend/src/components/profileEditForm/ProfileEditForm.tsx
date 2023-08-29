196f993b1bccd999e23da0d305289a44a18ce556
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-29 10:53:05 +0300

    Fix avatar uploader background height

--- frontend/src/components/profileEditForm/ProfileEditForm.tsx
+++ frontend/src/components/profileEditForm/ProfileEditForm.tsx
@@ -17,7 +17,6 @@ import CityInput from './CityInput';
 import DateInput from './DateInput';
 import { Controller } from 'react-hook-form';
 import { User } from 'types';
-import { useStore } from 'react-redux';
 import { DecodedUser } from 'types/models/User';
 import jwtDecode from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
