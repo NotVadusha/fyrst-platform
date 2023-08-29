3e755cf86186e61de1047079ce56536739fc58bc
Anton Poroshkin <poroshkinanton18@gmail.com>
2023-08-29 12:15:25 +0300

    Add more seeding data

--- backend/seeders/20230827152324-seed-booking.js
+++ backend/seeders/20230827152324-seed-booking.js
@@ -6,22 +6,33 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const bookings = [];
 
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < 20; i++) {
+      let status;
+      const startDate = faker.date.past();
+      const numberOfPositions = faker.number.int({ min: 1, max: 20 });
+      const positionsAvailable = faker.number.int({ min: 0, max: numberOfPositions });
+
+      if (positionsAvailable === 0) {
+        status = 'completed';
+      } else {
+        status = faker.helpers.arrayElement(['pending', 'rejected', 'canceled']);
+      }
+
       bookings.push({
-        status: 'accepted',
+        status: 'pending',
         numberOfPositions: faker.number.int({ min: 1, max: 20 }),
         facilitiesRate: 1,
-        createdBy: 1123,
+        createdBy: faker.number.int({ min: 1, max: 19 }),
         sex: faker.person.sex(),
         age: faker.number.int({ min: 18, max: 60 }),
-        education: faker.lorem.words(5),
+        education: faker.lorem.words(10),
         positionsAvailable: 1,
         workingHours: 1,
         pricePerHour: 0.01,
-        notes: faker.lorem.words(10),
-        facilityId: faker.number.int({ min: 1, max: 3 }),
-        startDate: faker.date.recent(),
-        endDate: faker.date.recent(),
+        notes: faker.lorem.paragraphs(3),
+        facilityId: faker.number.int({ min: 1, max: 19 }),
+        startDate,
+        endDate: faker.date.between({ from: startDate, to: new Date() }),
         updatedAt: new Date(),
       });
 
