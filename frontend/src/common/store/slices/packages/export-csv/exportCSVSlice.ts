7b944458b4ac2c9ad4ba57181453f4db8074a661
Roman Matviienko <hcetanutrof@gmail.com>
2023-09-01 01:05:59 +0300

    feat: added export-csv slice

---
+++ frontend/src/common/store/slices/packages/export-csv/exportCSVSlice.ts
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+interface ExportCsvPayload {
+  feature: string;
+  filters: any;
+}
+
+interface CsvState {
+  isLoading: boolean;
+}
+
+const initialState: CsvState = {
+  isLoading: false,
+};
+
+export const exportCSV = createAsyncThunk<void, ExportCsvPayload, {}>(
+  'exportCSV',
+  async ({ feature, filters }) => {
+    const params = new URLSearchParams();
+    const { REACT_APP_API_URL } = process.env;
+    const { limit, offset, ...otherFilters } = filters;
+
+    Object.keys(otherFilters).forEach(key => {
+      params.set(key, String(otherFilters[key]));
+    });
+
+    const response = await fetch(`${REACT_APP_API_URL}/${feature}/export-csv?${params}`);
+
+    if (!response.ok) {
+      throw new Error('Failed to download file');
+    }
+
+    const blob = await response.blob();
+    const contentDisposition = response.headers.get('Content-Disposition') || '';
+    const matches = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(contentDisposition);
+    const filename = matches != null && matches[1] ? matches[1] : `${feature}.csv`;
+
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  },
+);
+
+const exportCSVSlice = createSlice({
+  name: 'export-csv',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(exportCSV.pending, state => {
+      state.isLoading = true;
+    });
+    builder.addCase(exportCSV.fulfilled, state => {
+      state.isLoading = false;
+    });
+    builder.addCase(exportCSV.rejected, state => {
+      state.isLoading = false;
+    });
+  },
+});
+
+export const csvActions = exportCSVSlice.actions;
+export default exportCSVSlice.reducer;
