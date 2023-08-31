c3753c59b360d1b4f4c6b4dcfe7d571a0f94ff19
sl-chern <slavikchernog@gmail.com>
2023-08-31 15:42:17 +0300

    fix: deleted unnecessary console.log

--- frontend/src/pages/authentication/success-google-auth/SuccessGoogleAuthPage.tsx
+++ frontend/src/pages/authentication/success-google-auth/SuccessGoogleAuthPage.tsx
@@ -13,8 +13,6 @@ const SuccessGoogleAuthPage = () => {
     const userInfoJson = Cookies.get('user');
     const userInfo = JSON.parse(userInfoJson || '');
 
-    console.log(userInfo);
-
     dispatch(setUser(userInfo));
 
     localStorage.setItem('accessToken', accessToken!);
