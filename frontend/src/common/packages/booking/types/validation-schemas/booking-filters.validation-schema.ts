4cf7a3d73593f50ba2b58e9c3ea76c0c3412ecd6
NotVadusha <vadimkbondarchuk@gmail.com>
2023-09-05 23:01:09 +0300

    fix: default values for booking filter

--- frontend/src/common/packages/booking/types/validation-schemas/booking-filters.validation-schema.ts
+++ frontend/src/common/packages/booking/types/validation-schemas/booking-filters.validation-schema.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export const bookingFiltersSchema = yup.object({
-  facility: yup.string(),
+  facility: yup.lazy(value => (typeof value === 'number' ? yup.number() : yup.string())),
   endDate: yup.date(),
   startDate: yup.date(),
   status: yup.string().oneOf(['pending', 'accepted', 'rejected', 'canceled', 'completed']),
