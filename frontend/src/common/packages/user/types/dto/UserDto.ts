391dd6c92fea1ca6a49d6f6c90ad29f71903c362
NotVadusha <vadimkbondarchuk@gmail.com>
2023-09-06 01:52:04 +0300

    fix: updating user data with redunant fields

--- frontend/src/common/packages/user/types/dto/UserDto.ts
+++ frontend/src/common/packages/user/types/dto/UserDto.ts
@@ -7,15 +7,3 @@ export type UpdateUserBody = {
   birthdate?: string | null;
   document_number?: string | null;
 };
-
-export interface EditUserPage {
-  id: number;
-  phone_number: string;
-  first_name: string;
-  last_name: string;
-  email: string;
-  city: string;
-  birthdate: string;
-  document_number?: string | null;
-  role_id: number;
-}
