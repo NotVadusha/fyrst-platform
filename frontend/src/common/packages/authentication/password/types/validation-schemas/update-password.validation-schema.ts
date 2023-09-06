fe6081a6cdee9758627c5d71ee0936a6746dc810
NotVadusha <vadimkbondarchuk@gmail.com>
2023-09-06 12:00:33 +0300

    fix: security password validation

--- frontend/src/common/packages/authentication/password/types/validation-schemas/update-password.validation-schema.ts
+++ frontend/src/common/packages/authentication/password/types/validation-schemas/update-password.validation-schema.ts
@@ -4,13 +4,20 @@ export const updatePasswordSchema = y.object().shape({
   currentPassword: y.string().required('Current password is required'),
   newPassword: y
     .string()
-    .required('Password is required')
-    .notOneOf([y.ref('currentPassword')], "New password can't be the same as current password")
-    .matches(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+).{8,24}$/, {
-      message:
-        'Password must contain 8-24 characters, at least one uppercase, and at least one special character.',
-      excludeEmptyString: true,
-    }),
+    .min(8, 'Password must be at least 8 characters')
+    .max(24, 'Password can be a total of 24 characters')
+    .matches(
+      /^[a-zA-Z\d!"#$%&'()*+,-.\/:;<=>?@\[\\\]^_\`{|}~]+$/g,
+      'Password can only contain Latin letters and special characters',
+    )
+    .test('contains-spec-chars', 'Password must contain at least one special character', value => {
+      const specialChars = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+      for (const char of specialChars) {
+        if (value?.includes(char)) return true;
+      }
+      return false;
+    })
+    .required('Passoword is a required field'),
   confirmNewPassword: y
     .string()
     .oneOf([y.ref('newPassword')], 'Passwords must match')
