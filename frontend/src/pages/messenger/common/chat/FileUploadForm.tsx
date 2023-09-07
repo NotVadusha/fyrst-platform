7e33d04b68b88cfbaa04ecdf42098c0538b3c3dc
Dan <danyaolekhq@gmail.com>
2023-09-07 20:54:18 +0300

    fix: handle image size on fe

--- frontend/src/pages/messenger/common/chat/FileUploadForm.tsx
+++ frontend/src/pages/messenger/common/chat/FileUploadForm.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button } from 'src/common/components/ui/common/Button';
 import { toast } from 'src/common/components/ui/common/Toast/useToast';
 import { useAppDispatch } from 'src/common/hooks/redux';
 import { useUploadAttachmentMutation } from 'src/common/store/api/packages/chat/chatApi';
 import { setAttachment } from 'src/common/store/slices/packages/messenger/messangerSlice';
+
+const MAX_IMAGE_SIZE = 100 * 1024;
 
 export function FileUploadForm({ onUpload }: { onUpload: () => void }) {
   const [base64String, setBase64String] = useState<string | undefined>('');
@@ -25,32 +27,38 @@ export function FileUploadForm({ onUpload }: { onUpload: () => void }) {
       .catch(err => err);
   };
 
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.[0]) return;
+
+    const file = event.target.files?.[0];
+
+    if (file.size >= MAX_IMAGE_SIZE) {
+      toast({
+        title: 'Image is too large.',
+        description: 'Please, choose another image less than 100mb size',
+        variant: 'destructive',
+      });
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const result = event.target?.result as string;
+      const base64 = result.split(',')[1];
+      setBase64String(base64);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div className='flex flex-col gap-4'>
       <div className='grid w-full max-w-sm items-center gap-1.5'>
         <label htmlFor='picture' className='text-xl text-dark'>
           Picture
         </label>
-        <input
-          accept='image/*'
-          id='picture'
-          type='file'
-          onChange={event => {
-            if (!event.target.files?.[0]) return;
-            const file = event.target.files?.[0];
-            const reader = new FileReader();
-
-            reader.onload = function (event) {
-              const result = event.target?.result as string;
-              const base64 = result.split(',')[1];
-              setBase64String(base64);
-
-              console.log(base64);
-            };
-
-            reader.readAsDataURL(file);
-          }}
-        />
+        <input accept='image/*' id='picture' type='file' onChange={handleImageChange} />
       </div>
       <Button className='w-full' disabled={!base64String} onClick={() => uploadFile()}>
         Upload
