566d4282651759157d8bb75a1ac286b27eea3fb3
NotVadusha <vadimkbondarchuk@gmail.com>
2023-09-08 11:21:35 +0300

    fix: add type validation for dto

--- backend/src/packages/notification/dto/create-notification.dto.ts
+++ backend/src/packages/notification/dto/create-notification.dto.ts
@@ -1,15 +1,19 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateNotificationDto {
   @IsNotEmpty()
+  @IsString()
   readonly content: string;
 
   @IsNotEmpty()
+  @IsNumber()
   readonly recipientId: number;
 
   @IsNotEmpty()
+  @IsString()
   readonly type: string;
 
   @IsNotEmpty()
+  @IsNumber()
   readonly refId: number;
 }
