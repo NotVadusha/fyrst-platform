67c10aa6e2aa867d38082c2d5ccf6aaf1e4365ca
Dan <danyaolekhq@gmail.com>
2023-09-11 12:22:04 +0300

    fix: import

--- backend/test/invitation/invitation.service.spec.ts
+++ backend/test/invitation/invitation.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { InvitationService } from './invitation.service';
+import { InvitationService } from 'src/packages/invitation/invitation/invitation.service';
 
 describe('InvitationService', () => {
   let service: InvitationService;
