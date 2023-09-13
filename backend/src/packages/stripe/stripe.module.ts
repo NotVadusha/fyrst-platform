27495b12cf65db4f7c894432ffbb368dc4ea5e46
NotVadusha <vadimkbondarchuk@gmail.com>
2023-09-13 11:28:27 +0300

    fix: add notification service imports

--- backend/src/packages/stripe/stripe.module.ts
+++ backend/src/packages/stripe/stripe.module.ts
@@ -1,12 +1,12 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { StripeController } from './stripe.controller';
 import { StripeService } from './stripe.service';
-import { PaymentModule, UserProfileModule } from '..';
+import { NotificationModule, PaymentModule, UserProfileModule } from '..';
 
 @Module({
   controllers: [StripeController],
   providers: [StripeService],
-  imports: [forwardRef(() => PaymentModule), UserProfileModule],
+  imports: [forwardRef(() => PaymentModule), UserProfileModule, NotificationModule],
   exports: [StripeService],
 })
 export class StripeModule {}
