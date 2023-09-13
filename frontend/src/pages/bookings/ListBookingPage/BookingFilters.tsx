d7d8889f43ee51268073122809fd9d9da534735d
Roman Matviienko <hcetanutrof@gmail.com>
2023-09-13 11:50:06 +0300

    feat: added filters button on small screens

--- frontend/src/pages/bookings/ListBookingPage/BookingFilters.tsx
+++ frontend/src/pages/bookings/ListBookingPage/BookingFilters.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -7,6 +7,9 @@ import TextInput from '../../../common/components/ui/common/Input/common/TextInp
 import { Dropdown } from 'src/common/components/ui/common/Dropdown/Dropdown';
 import { useFetchFacilitiesQuery } from 'src/common/store/api/packages/facility/facilityApi';
 import { bookingFiltersSchema } from '../../../common/packages/booking/types/validation-schemas/booking-filters.validation-schema';
+import { Button } from '../../../common/components/ui/common/Button';
+import { ReactComponent as FiltersCloseIcon } from 'src/assets/icons/filters-close.svg';
+import { ReactComponent as FiltersOpenIcon } from 'src/assets/icons/filters-open.svg';
 
 type FormValues = yup.InferType<typeof bookingFiltersSchema>;
 
@@ -16,6 +19,7 @@ export const BookingFilters = ({
   handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
   const { data: facilities } = useFetchFacilitiesQuery({});
+  const [showFilters, setShowFilters] = useState(false);
   const options = facilities
     ? facilities.map(facility => ({
         label: facility.name,
@@ -31,75 +35,91 @@ export const BookingFilters = ({
   });
 
   return (
-    <Form {...form}>
-      <form className='w-full'>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
-          <div className='w-full '>
-            <label className='text-body-default text-blue font-medium' htmlFor='startDate'>
-              Status
-            </label>
-            <Dropdown
-              name='status'
-              control={form.control}
-              options={[
-                { label: 'All', value: '' },
-                { label: 'Pending', value: 'pending' },
-                { label: 'Accepted', value: 'accepted' },
-                { label: 'Rejected', value: 'rejected' },
-                { label: 'Canceled', value: 'canceled' },
-                { label: 'Completed', value: 'completed' },
-              ]}
-              styleVariant='shadows'
-              label=''
-              placeholder='Select Status'
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className='w-full '>
-            <label className='text-body-default text-blue font-medium' htmlFor='facility'>
-              Facility
-            </label>
-            <Dropdown
-              name='facility'
-              control={form.control}
-              options={options}
-              styleVariant='shadows'
-              label=''
-              placeholder='Select an option'
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className='w-full'>
-            <label className='text-body-default text-blue font-medium' htmlFor='startDate'>
-              Start date
-            </label>
-            <TextInput
-              name='startDate'
-              control={form.control}
-              type='date'
-              id='startDate'
-              label=''
-              onChange={handleInputChange}
-              styleVariant='shadows'
-            />
-          </div>
+    <div className='w-full'>
+      <Button
+        onClick={() => setShowFilters(!showFilters)}
+        variant='primary'
+        className='md:hidden justify-between shadow-lg w-full mb-5'
+      >
+        Filters:
+        {showFilters ? (
+          <FiltersCloseIcon className='w-[20px]' />
+        ) : (
+          <FiltersOpenIcon className='w-[20px]' />
+        )}
+      </Button>
+      <div className={`${showFilters || 'hidden'} md:block`}>
+        <Form {...form}>
+          <form className='w-full'>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
+              <div className='w-full '>
+                <label className='text-body-default text-blue font-medium' htmlFor='startDate'>
+                  Status
+                </label>
+                <Dropdown
+                  name='status'
+                  control={form.control}
+                  options={[
+                    { label: 'All', value: '' },
+                    { label: 'Pending', value: 'pending' },
+                    { label: 'Accepted', value: 'accepted' },
+                    { label: 'Rejected', value: 'rejected' },
+                    { label: 'Canceled', value: 'canceled' },
+                    { label: 'Completed', value: 'completed' },
+                  ]}
+                  styleVariant='shadows'
+                  label=''
+                  placeholder='Select Status'
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className='w-full '>
+                <label className='text-body-default text-blue font-medium' htmlFor='facility'>
+                  Facility
+                </label>
+                <Dropdown
+                  name='facility'
+                  control={form.control}
+                  options={options}
+                  styleVariant='shadows'
+                  label=''
+                  placeholder='Select an option'
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className='w-full'>
+                <label className='text-body-default text-blue font-medium' htmlFor='startDate'>
+                  Start date
+                </label>
+                <TextInput
+                  name='startDate'
+                  control={form.control}
+                  type='date'
+                  id='startDate'
+                  label=''
+                  onChange={handleInputChange}
+                  styleVariant='shadows'
+                />
+              </div>
 
-          <div className='w-full'>
-            <label className='text-body-default text-blue font-medium' htmlFor='endDate'>
-              End date
-            </label>
-            <TextInput
-              name='endDate'
-              control={form.control}
-              type='date'
-              id='endDate'
-              label=''
-              onChange={handleInputChange}
-              styleVariant='shadows'
-            />
-          </div>
-        </div>
-      </form>
-    </Form>
+              <div className='w-full'>
+                <label className='text-body-default text-blue font-medium' htmlFor='endDate'>
+                  End date
+                </label>
+                <TextInput
+                  name='endDate'
+                  control={form.control}
+                  type='date'
+                  id='endDate'
+                  label=''
+                  onChange={handleInputChange}
+                  styleVariant='shadows'
+                />
+              </div>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
   );
 };
--- frontend/src/pages/invoices/InvoicesFilters.tsx
+++ frontend/src/pages/invoices/InvoicesFilters.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useAppSelector } from 'src/common/hooks/redux';
 import { useLazyFetchWorkersByFacilityAdminIdQuery } from 'src/common/store/api/packages/timecards/timecardsApi';
@@ -20,6 +20,9 @@ import {
 import { RefreshButton } from 'src/common/components/ui/common/Button/common/refresh-button/RefreshButton';
 import TextInput from '../../common/components/ui/common/Input/common/TextInput/TextInput';
 import { userRoles } from 'shared/packages/roles/userRoles';
+import { Button } from '../../common/components/ui/common/Button';
+import { ReactComponent as FiltersCloseIcon } from 'src/assets/icons/filters-close.svg';
+import { ReactComponent as FiltersOpenIcon } from 'src/assets/icons/filters-open.svg';
 
 type InvoicesFiltersProps = {
   handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -41,6 +44,7 @@ export const InvoicesFilters: React.FC<InvoicesFiltersProps> = ({
   const user = useAppSelector(state => state.user);
   const userRoleId = useAppSelector(state => state.user.role_id);
   const [getPayees, { data: payees }] = useLazyFetchWorkersByFacilityAdminIdQuery();
+  const [showFilters, setShowFilters] = useState(false);
 
   useEffect(() => {
     if (user.id && user.role_id !== userRoles.WORKER) getPayees(user.id!);
@@ -57,101 +61,115 @@ export const InvoicesFilters: React.FC<InvoicesFiltersProps> = ({
     })) || [];
 
   return (
-    <>
-      <Form {...form}>
-        <form>
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-6 w-full'>
-            {userRoleId !== 1 ? (
+    <div className='w-full'>
+      <Button
+        onClick={() => setShowFilters(!showFilters)}
+        variant='primary'
+        className='md:hidden justify-between shadow-lg w-full mb-5'
+      >
+        Filters:
+        {showFilters ? (
+          <FiltersCloseIcon className='w-[20px]' />
+        ) : (
+          <FiltersOpenIcon className='w-[20px]' />
+        )}
+      </Button>
+      <div className={`${showFilters || 'hidden'} md:block`}>
+        <Form {...form}>
+          <form>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-6 w-full'>
+              {userRoleId !== 1 ? (
+                <div className='flex flex-col gap-y-2 w-full md:max-w-[204px]'>
+                  <label className='text-body-default text-blue font-medium' htmlFor='payee'>
+                    Payee
+                  </label>
+                  <FormField
+                    control={form.control}
+                    name='payee'
+                    render={({ field }) => (
+                      <FormItem>
+                        {/*eslint-disable-next-line */}
+                        {/*@ts-ignore*/}
+                        <FormControl>
+                          {/*eslint-disable-next-line */}
+                          {/*@ts-ignore*/}
+                          <Select onValueChange={value => handleSelectChange(value, 'payee')}>
+                            <FormControl>
+                              <SelectTrigger>
+                                <span className='font-semibold'>
+                                  <SelectValue placeholder='All' />
+                                </span>
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              <span className='font-semibold text-dark-blue'>
+                                <SelectItem value=''>All</SelectItem>
+                              </span>
+                              {payeeOptions.map(option => (
+                                <span className='font-semibold text-dark-blue' key={status}>
+                                  <SelectItem value={option.value.toString()}>
+                                    {option.label}
+                                  </SelectItem>
+                                </span>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              ) : null}
               <div className='flex flex-col gap-y-2 w-full md:max-w-[204px]'>
-                <label className='text-body-default text-blue font-medium' htmlFor='payee'>
-                  Payee
+                <label className='text-body-default text-blue font-medium' htmlFor='startDate'>
+                  Start date
                 </label>
                 <FormField
                   control={form.control}
-                  name='payee'
+                  name='minDate'
                   render={({ field }) => (
                     <FormItem>
                       {/*eslint-disable-next-line */}
                       {/*@ts-ignore*/}
-                      <FormControl>
-                        {/*eslint-disable-next-line */}
-                        {/*@ts-ignore*/}
-                        <Select onValueChange={value => handleSelectChange(value, 'payee')}>
-                          <FormControl>
-                            <SelectTrigger>
-                              <span className='font-semibold'>
-                                <SelectValue placeholder='All' />
-                              </span>
-                            </SelectTrigger>
-                          </FormControl>
-                          <SelectContent>
-                            <span className='font-semibold text-dark-blue'>
-                              <SelectItem value=''>All</SelectItem>
-                            </span>
-                            {payeeOptions.map(option => (
-                              <span className='font-semibold text-dark-blue' key={status}>
-                                <SelectItem value={option.value.toString()}>
-                                  {option.label}
-                                </SelectItem>
-                              </span>
-                            ))}
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
+                      <TextInput
+                        control={form.control}
+                        type='date'
+                        id='startDate'
+                        label=''
+                        {...field}
+                        onChange={handleInputChange}
+                      />
                     </FormItem>
                   )}
                 />
               </div>
-            ) : null}
-            <div className='flex flex-col gap-y-2 w-full md:max-w-[204px]'>
-              <label className='text-body-default text-blue font-medium' htmlFor='startDate'>
-                Start date
-              </label>
-              <FormField
-                control={form.control}
-                name='minDate'
-                render={({ field }) => (
-                  <FormItem>
-                    {/*eslint-disable-next-line */}
-                    {/*@ts-ignore*/}
-                    <TextInput
-                      control={form.control}
-                      type='date'
-                      id='startDate'
-                      label=''
-                      {...field}
-                      onChange={handleInputChange}
-                    />
-                  </FormItem>
-                )}
-              />
+              <div className='flex flex-col gap-y-2 w-full md:max-w-[204px]'>
+                <label className='text-body-default text-blue font-medium' htmlFor='endDate'>
+                  End date
+                </label>
+                <FormField
+                  control={form.control}
+                  name='maxDate'
+                  render={({ field }) => (
+                    <FormItem>
+                      {/*eslint-disable-next-line */}
+                      {/*@ts-ignore*/}
+                      <TextInput
+                        control={form.control}
+                        type='date'
+                        id='endDate'
+                        label=''
+                        {...field}
+                        onChange={handleInputChange}
+                      />
+                    </FormItem>
+                  )}
+                />
+              </div>
             </div>
-            <div className='flex flex-col gap-y-2 w-full md:max-w-[204px]'>
-              <label className='text-body-default text-blue font-medium' htmlFor='endDate'>
-                End date
-              </label>
-              <FormField
-                control={form.control}
-                name='maxDate'
-                render={({ field }) => (
-                  <FormItem>
-                    {/*eslint-disable-next-line */}
-                    {/*@ts-ignore*/}
-                    <TextInput
-                      control={form.control}
-                      type='date'
-                      id='endDate'
-                      label=''
-                      {...field}
-                      onChange={handleInputChange}
-                    />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-        </form>
-      </Form>
-    </>
+          </form>
+        </Form>
+      </div>
+    </div>
   );
 };
