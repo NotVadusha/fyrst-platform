8dd8aa9ac8ce6d975211ffee57d36da0739c7241
sl-chern <slavikchernog@gmail.com>
2023-09-14 14:49:55 +0300

    fix: fixed acces to payment page for platform admin

--- backend/src/packages/payment/payment.service.ts
+++ backend/src/packages/payment/payment.service.ts
@@ -54,8 +54,13 @@ export class PaymentService {
         },
       ],
     });
+    const user = await this.userService.findOne(userId);
     if (!payment) throw new NotFoundException('Payment not found');
-    if (payment.timecard.employee.id !== userId && payment.timecard.booking.createdBy !== userId)
+    if (
+      payment.timecard.employee.id !== userId &&
+      payment.timecard.booking.createdBy !== userId &&
+      user.role_id !== userRoles.PLATFORM_ADMIN
+    )
       throw new ForbiddenException('Access denied');
     delete payment.timecard.booking;
     return payment;
